c664af7f82846badedd595d47c2161a84c36f00a
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-07 14:07:13 +0000

    Add images in assets/images , adapte routes.rb in order to add confirm page to crud actions with database. changement of databases because an error occured.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/create'
-  get 'posts/new'
-  get 'posts/update'
-  get 'posts/show'
-  get 'posts/destroy'
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
