4fccd2745d4774968c63daadf301aa9e3355e57f
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-07 13:29:14 +0000

    implementation of functions create destroy new show index, add the function post_param for validation, use of bootstrap for pages look like

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,33 @@
 class PostsController < ApplicationController
   def index
-  end
-
-  def create
+    @posts = Post.all
   end
 
   def new
+    @post=Post.new
+  end
+ 
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path, notice: "J'ai créé un post !"
   end
 
   def update
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice:"J'ai supprimé le blog !"
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
