61d8e8ed0028a6777d5ca879143f95886a5c2b0f
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-07 13:42:34 +0000

    create file confirm.html.erb for confirmation and implemente all crud function in posts_controller with use of function before_action tu optmise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def confirm
+    @post= Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
   def index
     @posts = Post.all
   end
@@ -6,28 +13,48 @@ class PostsController < ApplicationController
   def new
     @post=Post.new
   end
- 
 
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to posts_path, notice: "J'ai créé un post !"
-  end
-
-  def update
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"J'ai supprimé le blog !"
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def destroy
+  def update
     @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path, notice:"J'ai supprimé le blog !"
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "J'ai édité le blog !"
+    else
+      render :edit
+    end
   end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+    if @post.save
+      redirect_to posts_path, notice: "J'ai créé un post !"
+    else
+      render :new
+    end
+  end
+end
+  def confirm
+    @post = Post.new(post_params)
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
