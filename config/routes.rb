cc27e941c9ee3ee75a0c22c8fd82145b3aebec53
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-07 12:41:32 +0000

    generate controller Posts and model Post with column content

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/new'
+  get 'posts/update'
+  get 'posts/show'
+  get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
